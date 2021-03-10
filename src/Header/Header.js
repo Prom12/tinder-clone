05c5be74d44012cf2afc3e44e658d6a6ab444850
prom12 <51293420+Prom12@users.noreply.github.com>
2021-03-10 17:40:40 +0000

    header icons done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,12 +2,17 @@ import React from 'react'
 import './Header.css' 
 import ProfileIcon from '@material-ui/icons/Person'
 import IconButton from '@material-ui/core/IconButton'
+import Forum from '@material-ui/icons/Forum'
 
 function Header() {
     return (
         <div className='header'>
             <IconButton>
                 <ProfileIcon fontSize='large' className='header_icon'/>
+            </IconButton>
+            <img className='header_logo' src='https://seeklogo.com/images/T/tinder-logo-FAAE852EC0-seeklogo.com.png'/>
+            <IconButton>
+                <Forum fontSize='large' className='header_icon'></Forum>
             </IconButton>
         </div>
     )
