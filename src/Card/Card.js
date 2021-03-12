e925b392322a8c53edd23f4e73f19e7c27626580
prom12 <51293420+Prom12@users.noreply.github.com>
2021-03-12 17:53:35 +0000

    axios installation

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,24 +1,16 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import TinderCard from 'react-tinder-card'
 import './Card.css'
-
+ import axios from './axios.js'
 function Card() {
-    const [info,setInfo] = useState(
-    [
-        {
-            name: 'Most Beautiful',
-            url: 'https://tipsforwomens.org/wp-content/uploads/2019/11/1572733720_Jare-Ijalana-is-Nigerian-quotthe-most-beautiful-girl-in-the-1024x584.jpg'
-        },
-        {
-            name: 'Ariana Grande',
-            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRln1TRVIBKRa8H-RtKPZ5IdbdkKiCW68Hh9Q&usqp=CAU'
-        },
-        {
-            name: 'Omotola jalade ekeinde',
-            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmAVLUhbX37WSWVQT_ikHZo2byyNYdLelc1g&usqp=CAU'
+    const [info,setInfo] = useState([]);
+    useEffect(()=>{
+        async function fetchData(){
+            const req = await axios.get('/cards');
+            setInfo(req.data);
         }
-    ]);
-
+        fetchData();
+    },[])
     const swiped =(direction,nameDelete)=>{
         console.log('removing '+ nameDelete);
     }
